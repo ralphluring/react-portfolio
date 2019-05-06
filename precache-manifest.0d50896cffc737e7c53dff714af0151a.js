886650366015cbb713ee97c96d484c45aa973700
Ralph Luring <bennettchrislloyd@gmail.com>
2019-05-06 02:54:48 -0700

    Updates

---
+++ precache-manifest.0d50896cffc737e7c53dff714af0151a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68f4284e3eb764cd62d259d47d4fb28",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b59b619007a54cedc76a",
+    "url": "/react-portfolio/static/css/main.ed74438b.chunk.css"
+  },
+  {
+    "revision": "5e999ddd30f4dc54a1aa",
+    "url": "/react-portfolio/static/js/2.b74ec4af.chunk.js"
+  },
+  {
+    "revision": "b59b619007a54cedc76a",
+    "url": "/react-portfolio/static/js/main.ca8053d7.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e3675444984f385548208a1d3a9b6ec.js"
+  "/react-portfolio/precache-manifest.0d50896cffc737e7c53dff714af0151a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
